5e55a981963a7f7a9c58ba0c03b27247794550a4
BryantVail <Bryanttv@outlook.com>
2015-11-17 18:35:54 -0500

    First Submission of unzipped file
    First submission from library work, done in classroom.

---
+++ PayrollSystemTestChapter12-11PieceWorkerEmployeeBryantVTues630/PayrollSystemTest/PieceWorker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+
+    class PieceWorker : Employee
+    {
+
+        //instance variables
+        decimal pieces;
+        decimal wage;
+
+
+        //Constructor()
+        public PieceWorker(string first, string last, string ssn, decimal wagePerPiece, decimal piecesProduced): base(first, last, ssn)
+        {
+            pieces = piecesProduced;
+            wage = wagePerPiece;
+
+        }//end Constructor(firstN, lastN, ssn, wage, peicesProduced)=count: 5 parameter constructor
+
+        public decimal Wage
+        {
+            get
+            {
+                return wage;
+            }//end get
+            set
+            {
+                if(value>=0)
+                {
+                    wage = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Wage", value, "Wage cannot be negative.");
+                }//end if (Property: Wage)
+            }//end set
+        }////end Property: Wage
+
+        public decimal Pieces
+        {
+            get
+            {
+                return pieces;
+            }// end get
+            set
+            {
+                if(value>=0)
+                {
+                    pieces = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Pieces", value, "Pieces Produced cannot be negative.");
+                }//end if
+            }//end set
+        }//end Property Pieces
+
+        public override decimal Earnings()
+        {
+
+            return Pieces * Wage;
+
+        }//end MEthod Earnings
+
+        public override string ToString()
+        {
+            return string.Format("Piece Worker Employee: {0}\n{1}: {2:C}; {3}: {4:F2}", base.ToString(),"Wage per piece produced: ", Wage.ToString(), "Pieces Produced", Pieces.ToString());
+
+                 /*
+                 * "hourly employee: {0}\n{1}: {2:C}; {3}: {4:F2}",
+         base.ToString(), "hourly wage", Wage, "hours worked", Hours );
+                 * */
+        }
+    }
+
---
+++ PayrollSystemTestChapter12-11PieceWorkerEmployeeBryantVTues630/PayrollSystemTest/TestSolution1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+
+    class TestSolution1
+    {
+        public static void Main( string [] args)
+        {
+            bool loop = true;
+            while(loop == true)
+            {
+                Console.WriteLine("Welcome to Employee Builder: ");
+                Console.Read();
+
+                //instantiate Employee Array
+                Employee[] Employees = new Employee[5];
+
+                //fill array with different 'Employee' Derived Types all functioning under their own version of the 'Employee' Methods.
+                Employees[0] = new BasePlusCommissionEmployee("Jerry", "Redmond", "000-00-0000", 500.00M, .8M, 700M);
+                Employees[1] = new CommissionEmployee("Carlos", "Reese", "111-11-1111", 999.00M, .45M);
+                Employees[2] = new HourlyEmployee("Mike", "Luongo", "222-22-2222", 15.00M, 45M);
+                Employees[3] = new PieceWorker("Marcus", "Shepherd", "333-33-3333", 3.50M, 100M);
+                Employees[4] = new SalariedEmployee("Larry", "Peters", "444-44-4444", 75000M);
+                //Employees[5] = new PieceWorker("Hakeem", "Rogers", "555-55-5555", 5.0M,1);
+
+
+
+                Console.WriteLine("Employees Earnings & System Signature through ToString: \n\n{0}\t\t{1}", "Earnings:", "String:");
+               
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("\n" + Employees[i].ToString() + "\t\t" + Employees[i].Earnings());
+                }
+
+                Console.ReadLine();
+
+            }//End while
+            
+            
+        }//end main
+    }
+
